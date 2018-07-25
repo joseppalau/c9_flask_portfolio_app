3fff541835a97b26d0fa9330f8e8d429972ef010
joseppalau <30823849+joseppalau@users.noreply.github.com>
2018-07-25 19:36:44 +0200

    dash

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def shopping_list_post():
               
             return render_template('shopping_list.html', result="\n".join([str(item) for item in shop_list]))
           except ValueError:
-            return "Easy now! Let's keep it simple! Just words with a space between them"
+            return "Easy now! Let's keep it simple! Just words with a dash between them"
           
   	      
 @app.route('/time', methods=['GET','POST'])
